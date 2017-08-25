880825bc71a5de6052f06e70679279b64849a590
Denis <stepanov.denis91@gmail.com>
2017-08-25 23:56:21 +0300

    Less debug output, error when no source files

--- utils/build_ll_to_exe2.sh
+++ utils/build_ll_to_exe2.sh
@@ -7,18 +7,16 @@ target_dir="`readlink -f $1`"
 cd "$(dirname "$0")/.."
 
 pushd $target_dir >nul
-echo "Compiling Jellyfish bitcode"
+echo "[ll2exe] Compiling Jellyfish bitcode"
 
 mkdir -p obj
 
 for f in ll-bitcode/*.ll
-do
-	echo "Compiling $f..";
-	
+do	
 	clang-cl -Wno-override-module -m64 -Z7 -MDd -c -o "obj/${f#ll-bitcode/}.o" "$f"
 done
 popd >nul
 
-echo "Linking executable"
+echo "[ll2exe] Linking executable"
 mkdir -p $target_dir/bin
 clang-cl -m64 -Z7 -MDd -Wl,-subsystem:console -o$target_dir/bin/jfprogram.exe $target_dir/obj/*.o build/runtime/obj/*.o "$LLVM_BUILD/Debug/shared/LLVM.lib"
